d6b24e9423f740f024524152113f420bdd7b2eb8
Amar pratap <amarprataphzb@gmail.com>
2024-11-08 12:17:20 +0530

    route handler

--- src/app.js
+++ src/app.js
@@ -1,29 +1,42 @@
 const express = require("express");
 const app = express();
 
-app.get("/user", (req, res) => {
-  res.send({ firstName: "Amar", lastName: "Pratap" });
+// app.get("/user", (req, res) => {
+
+//   res.send({ firstName: "Amar", lastName: "Pratap" });
+// });
+
+// app.post("/user", (req, res) => {
+//   // here is the code to submit the data to the backend
+//   res.send("Data successfully registered");
+// });
+
+// app.delete("/user", (req, res) => {
+//   // here is the code to submit the data to the backend
+//   res.send("Delete successfully");
+// });
+
+// app.put("/user", (req, res) => {
+//   // here is the code to submit the data to the backend
+//   res.send("data updated successfully");
+// });
+// app.patch("/user", (req, res) => {
+//   // here is the code to submit the data to the backend
+//   res.send("data patched successfully");
+// });
+
+
+
+app.use("/user", (req, res,next) => {
+  console.log("handling the route user")
+  // res.send("response1");
+  next();
+},(req,res)=>{
+  console.log("handling route user 2")
+  res.send("response2");
 });
 
-app.post("/user", (req, res) => {
-  // here is the code to submit the data to the backend
-  res.send("Data successfully registered");
+app.listen(8080, () => {
+  console.log("Server is listening on port 8080");
 });
-
-app.delete("/user", (req, res) => {
-    // here is the code to submit the data to the backend
-    res.send("Delete successfully");
-  });
-
-  app.put("/user", (req, res) => {
-    // here is the code to submit the data to the backend
-    res.send("data updated successfully");
-  });
-  app.patch("/user", (req, res) => {
-    // here is the code to submit the data to the backend
-    res.send("data patched successfully");
-  });
-
-app.listen(3000, () => {
-  console.log("Server is listening on port 3000");
-});
+ 
