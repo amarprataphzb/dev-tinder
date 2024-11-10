ca8677fbf011a24550fd6c8334fb6eb640a2b4c5
Amar pratap <amarprataphzb@gmail.com>
2024-11-10 15:59:50 +0530

    episode 5 before middleware

--- src/app.js
+++ src/app.js
@@ -25,18 +25,57 @@ const app = express();
 //   res.send("data patched successfully");
 // });
 
+// episode-4 notes
+
+//next() is the function which is responsible to execute the next route handler .agar last wala handler me bhi next() raha to wo uske next ko dundega aur agar nahi mila to error dega
+
+// app.use(
+//   "/user",
+//   (req, res, next) => {
+//     console.log("handling the route user");
+//     // res.send("response1");
+//     next();
+//   },
+//   (req, res, next) => {
+//     console.log("handling route user 2");
+//     res.send("response2");
+//     next();
+//   },
+//   (req, res, next) => {
+//     console.log("handling route user 2");
+//     res.send("response2");
+//     next();
+//   },
+//   (req, res, next) => {
+//     console.log("handling route user 2");
+//     res.send("response2");
+//     next();
+//   },
+//   (req, res) => {
+//     console.log("handling route user 2");
+//     res.send("response2");
+//   }
+// );
 
 
-app.use("/user", (req, res,next) => {
-  console.log("handling the route user")
-  // res.send("response1");
-  next();
-},(req,res)=>{
-  console.log("handling route user 2")
-  res.send("response2");
-});
+
+//Episode -5 
+
+app.get("/admin/getAllData", (req, res)=>{
+  //logic of checking the request is authorized
+  const token="xyzgsthyjuikjhgf"
+  const isAdminAuthorized =token==="xyz";
+  if(isAdminAuthorized){
+    res.send("Get All Data")
+  }else{
+    res.send("Unauthorized")
+  }
+})
+
+app.get("/admin/deleteUser",(req,res)=>{
+    res.send("Deleted a user")
+})
 
 app.listen(8080, () => {
   console.log("Server is listening on port 8080");
 });
- 
